7f75061be75ec4c5ff7dfe9968874bf70f6afc8a
melika <melikatavakoli13@gmail.com>
2026-01-30 01:57:37 +0330

    added types

--- ticket/types.py
+++ ticket/types.py
@@ -1,15 +1,6 @@
 from django.db import models
 
-class PriorityType(models.TextChoices):
-    low = "low", "Low"
-    high = "high", "High"
-    force = "force", "Force"
-    
-class TicketStatus(models.TextChoices):
-    open = "open", "Open"
-    close = "close", "Close"
-    
-class TicketCategory(models.TextChoices):
-    technical = "technical", "Technical"
-    course = "course", "Course"
-    mentoring = "mentoring", "Mentoring"
+class CHAT_TYPE_CHOICES(models.TextChoices):
+    private = "private", "Private"
+    group = "group", "Group"
+    channel = "channel", "Channel"
