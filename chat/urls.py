5ffec81bce2b950cdd09caaa5c64a06dce8f19a5
Melika Tavakoli <122203275+melikatavakoli@users.noreply.github.com>
2026-05-03 15:43:19 +0330

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -1,22 +1,12 @@
+# urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from chat import views
+from .views import ChatViewSet, MessageViewSet
 
-app_name = 'ticket'
 router = DefaultRouter()
-
-message_list = views.MessageViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
- 
-router.register(r'profiles', views.ProfileViewSet, basename='profile')
-router.register(r'chats', views.ChatViewSet, basename='chat')
-router.register(r'messages', views.MessageViewSet, basename='message')
+router.register('chats', ChatViewSet, basename='chat')
+router.register('messages', MessageViewSet, basename='message')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('create-chat-messages/<uuid:chat_id>/', views.MessageCreateView.as_view(), name='chat-messages-create'),
-    path('chats-messages/<uuid:chat_id>/', message_list, name='chat-messages'),
-
 ]
