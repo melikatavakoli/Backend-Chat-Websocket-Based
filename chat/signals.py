88e18aab73e5183bc9d75cd8932d41fbf01c479b
Melika Tavakoli <122203275+melikatavakoli@users.noreply.github.com>
2026-05-03 15:40:00 +0330

    clean code

--- chat/signals.py
+++ chat/signals.py
@@ -12,18 +12,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
-
-# @receiver(post_save, sender=Message)
-# def chat_message_notification(sender, instance, created, **kwargs):
-#     if created and instance.chat:
-#         from notifications.models import Logs
-#         # نوتیف برای همه اعضای چت به جز فرستنده
-#         recipients = instance.chat.active_memberships.exclude(user=instance.sender)
-#         for member in recipients:
-#             Logs.objects.create(
-#                 user=member.user,
-#                 title=f"پیام جدید در چت {instance.chat.name}",
-#                 description=instance.content[:100],
-#                 type="CHAT_MESSAGE",
-#                 related_chat=instance
-#             )
