af4855f91d63157188a7943d9b05215bec10970c
Melika Tavakoli <122203275+melikatavakoli@users.noreply.github.com>
2026-05-07 22:30:19 +0330

    Update admin.py

--- chat/admin.py
+++ chat/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from import_export import fields
 from import_export.admin import ImportExportModelAdmin
 from .models import (
-    Ticket,
-    TicketDetail,
     Profile,
     Chat,
     ChatMembership,
